4ae473a0d96217fcfe4444bb2f3044f4e6b672c1
Kahlil Nuñez Bayuk <kah.nunez@duocuc.cl>
2023-10-30 23:41:21 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='h-screen flex items-center justify-center'>
+        <p className='text-4xl text-center p-5'>Hola mundo Tailwindcss</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
