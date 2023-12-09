f85f905ff14a4d944540cbd292cf8bb5e3f5cae3
Kahlil Nuñez Bayuk <kah.nunez@duocuc.cl>
2023-12-09 03:49:31 -0300

    KN - Se agregan estilos e iconos en el header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+import { LuLogOut } from "react-icons/lu";
 
 export const Home = () => {
   return (
@@ -10,20 +12,19 @@ export const Home = () => {
         </div>
         <nav>
           <ul className="flex text-base [&>li>a]:inline-block [&>li>a]:px-4">
-            <li><a href="">Model S</a></li>
-            <li><a href="">Model 3</a></li>
-            <li><a href="">Model X</a></li>
-            <li><a href="">Model Y</a></li>
-            <li><a href="">Powerwall</a></li>
-            <li><a href="">Carga</a></li>
+            <li><a href="" className='hover:text-cyan-500 transition-colors duration-300'>Nosotros</a></li>
+            <li><a href="" className='hover:text-cyan-500 transition-colors duration-300'>Modelos</a></li>
+            <li><a href="" className='hover:text-cyan-500 transition-colors duration-300'>Servicios</a></li>
+            <li><a href="" className='hover:text-cyan-500 transition-colors duration-300'>Contáctanos</a></li>
           </ul>
         </nav>
         <nav className="flex flex-grow justify-end basis-0">
           <ul className="flex text-base [&>li>a]:inline-block [&>li>a]:px-4">
-            <li><a href="">Soporte</a></li>
-            <li><a href="">Tienda</a></li>
-            <li><a href="">Cuenta</a></li>
-            <li><a href="">Menú</a></li>
+            <li><a href="" ><Link to="/login" className='hover:text-cyan-500 transition-colors duration-300'>Ingresar</Link></a></li>
+            <li><a href="" ><Link to="/register" className='hover:text-cyan-500 transition-colors duration-300'>Registrarse</Link></a></li>
+            <li><a href="" className='hover:text-cyan-500 transition-colors duration-300'>Soporte</a></li>
+            <li><a href="" className='hover:text-cyan-500 transition-colors duration-300'>Tienda</a></li>
+            <li><a href=""><LuLogOut className='text-2xl hover:text-cyan-500 transition-colors duration-300' /></a></li>
           </ul>
         </nav>
       </header>
